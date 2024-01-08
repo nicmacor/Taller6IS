faa5a20ca6035d013912de28b2b0b8c4cf993b4a
nicmacor <93363380+nicmacor@users.noreply.github.com>
2024-01-08 17:51:53 -0500

    Taller terminadisimo

--- features/steps/toDoList_steps.py
+++ features/steps/toDoList_steps.py
@@ -20,14 +20,14 @@ def step_task_should_be_added_to_to_do_list(context, name):
 
 
 @given('the to-do list contains tasks')
-def step_impl(context):
+def step_to_do_list_contains_task(context):
     toDoList.add_task("Buy groceries", "Buy food and snacks")
     toDoList.add_task("Pay bills", "Pay rent and ensurance")
 
 
 #3.The user marks a task as completed
 @when('the user marks task "{task_index:d}" as completed')
-def step_impl(context, task_index):
+def step_user_marks_task_completed(context, task_index):
     toDoList.mark_complete(task_index)
 
 @then('the to-do list should show task "{task_index:d}" as completed')
@@ -37,40 +37,40 @@ def step_impl(context, task_index):
 
 #4. The user wants to list the incompleted tasks
 @when('the user list all incompleted tasks')
-def step_impl(context):
+def step_user_list_all_incompleted_tasks(context):
     context.list_incomplete_output = toDoList.list_incomplete_tasks()
 
 @then('the output should contain all incompleted tasks')
-def step_impl(context):
+def step_output_contain_incompleted_tasks(context):
     assert context.list_incomplete_output is not None , "No tasks were listed"
 
 ##2.List the tasks in the to-do List
 @when('the user list all tasks')
-def step_impl(context):
+def step_list_all_tasks(context):
     context.listOutput = toDoList.list_tasks()
 
 @then('the output should contain all tasks')
-def step_impl(context):
+def step_output_contain_all_tasks(context):
     assert context.listOutput is not None, f"No tasks were listed"
 
 #5.The user edits a task in the to-do list
 
 @when('the user edits task "{task_index:d}" to "{new_name}" and "{new_description}"')
-def step_impl(context, task_index, new_name, new_description):
+def step_edit_task(context, task_index, new_name, new_description):
     context.new_name=new_name
     context.new_description=new_description
     toDoList.edit_task(task_index, new_name, new_description)
 
 @then('the name and description of task "{task_index:d}" should be updated')
-def step_impl(context, task_index):
+def step_task_edited_updated(context, task_index):
     task = toDoList.tasks[task_index-1]
     assert context.new_name == task.name, f"Description of task '{task_index}' was not updated as expected to '{context.new_description}'"
 
 #6. the user clears all tasks
 @when('the user clears the to-do list')
-def step_impl(context):
+def step_clear_to_do_list(context):
     toDoList.clear_tasks()
 
 @then('the to-do list should be empty')
-def step_impl(context):
+def step_to_do_list_cleared(context):
     assert len(toDoList.tasks) == 0, "The to-do list is not empty after clearing"
